9560084bf28a0e5151bd14ce9e9f59e8b0b0239f
port <portification@gmail.com>
2016-03-16 13:28:31 +0100

    Add compass

--- src/3DMG/display.cs
+++ src/3DMG/display.cs
@@ -1,3 +1,30 @@
+function vectorToYawAngle(%vector)
+{
+	return mATan(getWord(%vector, 1), getWord(%vector, 0));
+}
+
+function angleToCompass(%angle)
+{
+    %val = mFloor((mRadToDeg(%angle) / 22.5) + 0.5);
+	%arr0  = "N";
+	%arr1  = "NNE";
+	%arr2  = "NE";
+	%arr3  = "ENE";
+	%arr4  = "E";
+	%arr5  = "ESE";
+	%arr6  = "SE";
+	%arr7  = "SSE";
+	%arr8  = "S";
+	%arr9  = "SSW";
+	%arr10 = "SW";
+	%arr11 = "WSW";
+	%arr12 = "W";
+	%arr13 = "WNW";
+	%arr14 = "NW";
+	%arr15 = "NNW";
+	return %arr[%val % 16];
+}
+
 function Player::TDMGDisplayLoop(%this)
 {
 	cancel(%this.TDMGDisplayLoop);
@@ -34,6 +61,7 @@ function Player::TDMGDisplayLoop(%this)
 	%color = %this.TDMGGas > 5000 * 0.1 ? "<color:DDDDDD>" : "<color:AA0000>";
 	%gas = "<color:DDDDDD><just:right>Gas:" @ %color SPC mCeil(%this.TDMGGas) @ " ";
 	%speed = "<color:DDDDDD><just:right>Speed: " @ mFloor(vectorLen(%this.getVelocity()) * 10) @ " ";
+	%speed = %speed @ angleToCompass(vectorToYawAngle(%this.getForwardVector())) @ " ";
 	%text = %pref @ %dist @ "<br><br><br><font:Times:20>" @ %blades @ %gas @ %spares @ %speed;
 	%this.client.centerPrint(%text, 1);
 	%this.TDMGDisplayLoop = %this.schedule(16, "TDMGDisplayLoop");
